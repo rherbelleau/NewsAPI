9c2d3193f75f12552d0a45c76ce49d0ce6eafd0a
Herbelleau Romain <71643592+rherbelleau@users.noreply.github.com>
2020-11-26 17:21:07 +0100

    splash_activity

--- app/src/main/java/com/example/newsapi/SplashScreenActivity.kt
+++ app/src/main/java/com/example/newsapi/SplashScreenActivity.kt
@@ -1,11 +1,21 @@
 package com.example.newsapi
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreenActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 3000 //3sec
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        //ouverture après x secondes
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },SPLASH_TIME_OUT)
     }
 }
